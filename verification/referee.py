c48297f386dabd04acbe4494d59a46a476e0f06d
MagiMaster <archmageomega@hotmail.com>
2014-04-23 17:55:48 -0500

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -29,8 +29,6 @@ checkio.referee.cover_codes
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
 
