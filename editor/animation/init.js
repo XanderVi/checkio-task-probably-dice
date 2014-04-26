1ba139dddf8b2ecd9b48c650e4e4f4c334308096
MagiMaster <archmageomega@hotmail.com>
2014-04-25 19:51:48 -0500

    Manually merging changes

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -40,16 +40,26 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
 
             var checkioInput = data.in;
+            var checkioInputStr = JSON.stringify(checkioInput);
 
-            if (data.error) {
-                $content.find('.call').html('Fail: checkio(' + JSON.stringify(checkioInput) + ')');
-                $content.find('.output').html(data.error.replace(/\n/g, ","));
+            var failError = function(dError) {
+                $content.find('.call').html('Fail: checkio(' + checkioInputStr + ')');
+                $content.find('.output').html(dError.replace(/\n/g, ","));
 
                 $content.find('.output').addClass('error');
                 $content.find('.call').addClass('error');
                 $content.find('.answer').remove();
                 $content.find('.explanation').remove();
                 this_e.setAnimationHeight($content.height() + 60);
+            };
+
+            if (data.error) {
+                failError(data.error);
+                return false;
+            }
+
+            if (data.ext && data.ext.inspector_fail) {
+                failError(data.ext.inspector_result_addon);
                 return false;
             }
 
